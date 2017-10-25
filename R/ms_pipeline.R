51e99bf3f6c759c2817411d2317c512182b18df1
muschellij2 <muschellij2@gmail.com>
2017-10-25 01:25:26 -0400

    fixing some of the man files

--- R/ms_pipeline.R
+++ R/ms_pipeline.R
@@ -1,4 +1,4 @@
-ms_pipeline = function(
+smri_pipeline = function(
   x,
   gold_standard = NULL,
   gs_space = NULL,
@@ -8,8 +8,8 @@ ms_pipeline = function(
   brain_mask = NULL,
   gs_interpolator = "NearestNeighbor",
   num_templates = 15,
-  malf_transform = "SynAggro",
-  ) {
+  malf_transform = "SynAggro"
+) {
 
 
   proc = bc_noneck_reduce(
@@ -28,27 +28,36 @@ ms_pipeline = function(
     verbose = verbose)
 
 
-  ind = seq(num_templates)
-  templates = malf.templates::malf_images()
-  images = templates$images[ind]
-  masks = templates$masks[ind]
-
   if (!is.null(brain_mask)) {
-    brain_mask = file.path(
-    outdir,
-    "Brain_Mask.nii.gz")
-  malf_result = malf(
-    infile = reg$T1,
-    template.images = images,
-    template.structs = masks,
-    interpolator = "genericLabel",
-    typeofTransform = malf_transform,
-    verbose = verbose,
-    func = "mode",
-    retimg = FALSE,
-    ...
-  )
-  mask = malf_result$outimg
+    brain_mask = check_nifti(brain_mask)
+  } else {
+    brain_mask_file = file.path(
+      outdir,
+      "Brain_Mask.nii.gz")
+    if (file.exists(brain_mask_file)) {
+      warning("Using brain mask file in outdir")
+      brain_mask = readnii(brain_mask)
+    } else {
+      ind = seq(num_templates)
+      templates = malf.templates::malf_images()
+      images = templates$images[ind]
+      masks = templates$masks[ind]
+
+      malf_result = malf(
+        infile = reg$T1,
+        template.images = images,
+        template.structs = masks,
+        interpolator = "genericLabel",
+        typeofTransform = malf_transform,
+        verbose = verbose,
+        func = "mode",
+        retimg = FALSE,
+        ...
+      )
+      brain_mask = malf_result$outimg
+    }
+
+  }
 
 
 }
