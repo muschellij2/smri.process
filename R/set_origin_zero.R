f18e921dfda5b28fc9a7d9bd012a2be08be08d8f
muschellij2 <muschellij2@gmail.com>
2018-04-28 14:52:11 -0400

    Added setting origin to zero to the mix.

---
+++ R/set_origin_zero.R
@@ -0,0 +1,23 @@
+#' Set Origin to Zero for the Images
+#'
+#' @param fnames character vector of filenames
+#'
+#' @return Logical indicating if the setting was successful
+#'
+#' @export
+#' @importFrom ANTsRCore antsImageRead antsSetOrigin antsGetOrigin
+set_origin_zero = function(fnames) {
+  stopifnot(is.character(fnames))
+  res = sapply(fnames, function(fname) {
+    img = antsImageRead(fname)
+    orig_origin = antsGetOrigin(img)
+    if (all(orig_origin == 0)) {
+      return(TRUE)
+    }
+    antsSetOrigin(img, origin = c(0, 0, 0))
+    antsImageWrite(img, filename = fname)
+    img_again = antsImageRead(fname)
+    all(antsGetOrigin(img_again) == c(0, 0, 0)   )
+  })
+  return(res)
+}
